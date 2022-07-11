42bff2a3eaaed3acc172251810427bdc786fad7e
Eraj Gillani <eraj.gillani@hotmail.com>
2022-07-11 18:53:24 -0400

    rid of old code, last commit before adding alembic

--- app/routers/post.py
+++ app/routers/post.py
@@ -48,8 +48,6 @@ def get_posts(id: int, db: Session = Depends(get_db), current_user: object = Dep
     if not post: 
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
 
-    # if post.user_id != current_user.id:
-    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Not authorized to perform requested action")
     return  post
 
 #for delete technically you shouldn't send any data back for 204
