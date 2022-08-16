026db6dbd6910a8f0f93171b04ba4d7ff5f11449
Eraj Gillani <eraj.gillani@hotmail.com>
2022-08-16 09:55:08 -0400

    fixing instruction message on home page

--- app/main.py
+++ app/main.py
@@ -81,7 +81,7 @@ asset_link_file = './assetlinks.json'
 @app.get("/", include_in_schema=False) 
 def root(): 
     return {"message": "Welcome to my post application", 
-            "instructions" : f"""type _/docs_ in the url to access the api schema"""} 
+            "instruction" : f"type /docs in the url to access the api documentation"} 
 
 @app.get("/.well-known/assetlinks.json", response_class=FileResponse, include_in_schema=False)
 def suppress_google_warnings():
